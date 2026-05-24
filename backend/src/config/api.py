7684aedbdc78306de821fb410cbcc62b4a563b00
forth <forthfora@gmail.com>
2026-05-24 23:41:34 +0100

    Playing around with Django

--- backend/src/config/api.py
+++ backend/src/config/api.py
@@ -1,5 +1,4 @@
 from ninja import NinjaAPI
-from apps.companies.api import router as companies_router
 from apps.accounts.api import router as accounts_router
 
 api = NinjaAPI(
--- backend/src/config/wsgi.py
+++ backend/src/config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for evp_website project.
+WSGI config for EVP website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evp_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
