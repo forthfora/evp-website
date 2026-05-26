1e82a6c9f8834d931dcc98de0f24eba1297311ff
forth <forthfora@gmail.com>
2026-05-26 21:26:35 +0100

    Better layout

--- frontend/src/app/app-layout.ui.tsx
+++ frontend/src/app/app-layout.ui.tsx
@@ -16,47 +16,49 @@ export function AppLayout() {
 	];
 
 	return (
-		<div className="bg-main text-contrast flex h-screen w-screen flex-col overflow-hidden transition-colors duration-200">
-			<header className="sticky top-0 z-50 flex items-center justify-between p-6 shadow-sm">
-				{/* Title & Logo Container */}
-				<div className="flex items-center gap-3 text-xl font-bold tracking-tight">
-					{/* Logo Image */}
-					<img
-						src={evpLogo}
-						alt="Edinburgh VenturePoint Logo"
-						className="h-8 w-auto object-contain"
-					/>
+		<div className="scrollbar-track-background scrollbar-thumb-foreground-muted bg-background text-foreground flex min-h-screen w-full scrollbar-thin font-sans transition-colors duration-200">
+			<div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col px-6">
+				<header className="bg-background sticky top-0 z-50 flex shrink-0 items-center justify-between py-6">
+					{/* Title & Logo Container */}
+					<div className="flex items-center gap-3 text-xl font-bold tracking-tight">
+						{/* Logo Image */}
+						<img
+							src={evpLogo}
+							alt="Edinburgh VenturePoint Logo"
+							className="h-12 w-auto object-contain"
+						/>
 
-					{/* Title Text */}
-					<span>Edinburgh VenturePoint</span>
-				</div>
+						{/* Title Text */}
+						<span className="font-title">Edinburgh VenturePoint</span>
+					</div>
 
-				{/* Links */}
-				<nav className="flex items-center gap-8">
-					{navLinks.map((link) => (
-						<Link
-							key={link.path}
-							to={link.path}
-							className="content-center font-medium no-underline"
+					{/* Links */}
+					<nav className="flex items-center gap-8">
+						{navLinks.map((link) => (
+							<Link
+								key={link.path}
+								to={link.path}
+								className="content-center font-medium no-underline"
+							>
+								{link.label}
+							</Link>
+						))}
+
+						{/* Theme Toggle Button */}
+						<button
+							onClick={toggleTheme}
+							className="flex cursor-pointer items-center justify-center border-none bg-transparent p-2 hover:opacity-80"
+							aria-label="Toggle theme"
 						>
-							{link.label}
-						</Link>
-					))}
+							{theme === 'dark' ? <Moon size={18} /> : <Sun size={18} />}
+						</button>
+					</nav>
+				</header>
 
-					{/* Theme Toggle Button */}
-					<button
-						onClick={toggleTheme}
-						className="flex cursor-pointer items-center justify-center border-none bg-transparent p-2 hover:opacity-80"
-						aria-label="Toggle theme"
-					>
-						{theme === 'dark' ? <Moon size={18} /> : <Sun size={18} />}
-					</button>
-				</nav>
-			</header>
-
-			<main className="custom-sz flex-1 overflow-y-auto">
-				<Outlet />
-			</main>
+				<main className="">
+					<Outlet />
+				</main>
+			</div>
 		</div>
 	);
 }
