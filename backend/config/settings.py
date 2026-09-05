8c9cb4d3ba011ef4397bdf0c936e0fd5489a9d0a
forth <forthfora@gmail.com>
2026-09-05 12:22:45 +0100

    Disable HTTPS for tests

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,8 +30,10 @@ ALLOWED_HOSTS = config(
     cast=lambda v: [s.strip() for s in v.split(",") if s.strip()],
 )
 
+RUNNING_TESTS = "test" in sys.argv
+
 # Security Settings for Production
-if not DEBUG:
+if not DEBUG and not RUNNING_TESTS:
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default=True, cast=bool)
     SESSION_COOKIE_SECURE = True
